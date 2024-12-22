2ee8fcf4efd204fd8f5b2409c90ab4cd02a8bb9f
Vo Thuong Nguyen <9839951+0xRoost3r@users.noreply.github.com>
2024-12-22 18:47:45 +0700

    feat: add Vercel deployment button to layout
    - Introduced a fixed Vercel deployment button in the layout for easy project cloning and deployment.
    - This addition enhances user accessibility to deploy the application directly from the interface.

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
         <div className="max-w-md mx-auto border border-black">
           <OnchainProviders>{children}</OnchainProviders>
           <Toaster/>
+          <div className="fixed bottom-6 right-6">
+            <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F0xRoost3r%2Fremake-wallet.git&env=NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,NEXT_PUBLIC_CDP_API_KEY,NEXT_PUBLIC_ENVIRONMENT,NEXT_PUBLIC_WC_PROJECT_ID,NEXT_PUBLIC_DEFAULT_CHAIN_ID&project-name=remake-walelt&repository-name=remake-wallet"><img src="https://vercel.com/button" alt="Deploy with Vercel"/></a>
+          </div>
         </div>
       </body>
     </html>
