94a33b7f00bcb626a09810e13c6be89d9a3c3cdd
Vo Thuong Nguyen <9839951+0xRoost3r@users.noreply.github.com>
2024-12-22 18:16:21 +0700

    Edit config public url

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://pi-smart-wallet.vercel.app';
+    : 'https://remakewallet.dappbuilder.fun';
 // Add your API KEY from the Coinbase Developer Portal
 export const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
 export const NEXT_PUBLIC_WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
